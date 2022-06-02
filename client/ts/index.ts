1a1dd0f67efe08679f6e2e1ad3c293024007b8d2
kito0 <drewshah0@gmail.com>
2022-06-01 22:19:32 -0600

    feat: add LinkProps & ImageProps interfaces

--- client/ts/index.ts
+++ client/ts/index.ts
@@ -1,1 +1,3 @@
-export type { Layout } from 'ts/interfaces';
+export type { LayoutProps } from 'ts/interfaces';
+export type { LinkProps } from 'ts/interfaces';
+export type { ImageProps } from 'ts/interfaces';
--- client/ts/interfaces.ts
+++ client/ts/interfaces.ts
@@ -1,3 +1,15 @@
-export interface Layout {
+export interface LayoutProps {
+  children: React.ReactNode;
+}
+
+export interface LinkProps {
+  href: string;
+  children: React.ReactNode;
+}
+
+export interface ImageProps {
+  src: string;
+  alt: string;
+  className?: string;
   children: React.ReactNode;
 }
