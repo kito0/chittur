989bcb8af4dc1d3291b3b101a7cb3a1ee8feaec9
kito0 <drewshah0@gmail.com>
2022-06-02 20:57:19 -0600

    refactor: update studio for ts

--- studio/schemas/post.ts
+++ studio/schemas/post.ts
@@ -21,21 +21,19 @@ export default {
       name: 'author',
       title: 'Author',
       type: 'reference',
-      to: {type: 'author'},
+      to: { type: 'author' },
     },
     {
       name: 'mainImage',
       title: 'Main image',
       type: 'image',
-      options: {
-        hotspot: true,
-      },
+      options: { hotspot: true },
     },
     {
       name: 'categories',
       title: 'Categories',
       type: 'array',
-      of: [{type: 'reference', to: {type: 'category'}}],
+      of: [{ type: 'reference', to: { type: 'category' } }],
     },
     {
       name: 'publishedAt',
@@ -56,10 +54,10 @@ export default {
       media: 'mainImage',
     },
     prepare(selection) {
-      const {author} = selection
+      const { author } = selection;
       return Object.assign({}, selection, {
         subtitle: author && `by ${author}`,
-      })
+      });
     },
   },
-}
+};
---
+++ studio/schemas/schema.ts
@@ -0,0 +1,12 @@
+import schemaTypes from 'all:part:@sanity/base/schema-type';
+import createSchema from 'part:@sanity/base/schema-creator';
+
+import author from './author';
+import blockContent from './blockContent';
+import category from './category';
+import post from './post';
+
+export default createSchema({
+  name: 'default',
+  types: schemaTypes.concat([post, author, category, blockContent]),
+});
