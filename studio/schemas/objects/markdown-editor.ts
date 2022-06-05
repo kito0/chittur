6063159e6dae2a2bfaf57087742538cdf73713d9
kito0 <drewshah0@gmail.com>
2022-06-04 20:30:28 -0600

    feat: create markdown-editor

---
+++ studio/schemas/objects/markdown-editor.ts
@@ -0,0 +1,12 @@
+export const markdownEditor = {
+  name: 'markdownEditor',
+  title: 'Markdown',
+  type: 'object',
+  fields: [
+    {
+      name: 'content',
+      title: 'Content',
+      type: 'markdown',
+    },
+  ],
+};
