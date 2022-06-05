ac1d30b88f37009ca771aeb38876f762becfcefc
kito0 <drewshah0@gmail.com>
2022-06-04 20:32:03 -0600

    feat: create accordion.ts

---
+++ studio/schemas/objects/accordion.ts
@@ -0,0 +1,17 @@
+export const accordion = {
+  name: 'accordion',
+  title: 'Accordion',
+  type: 'object',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'content',
+      title: 'Content',
+      type: 'markdown',
+    },
+  ],
+};
