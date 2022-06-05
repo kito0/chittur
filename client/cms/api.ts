48ae14705ec1b6aec1e0854a3bcfc4a0c7cddc52
kito0 <drewshah0@gmail.com>
2022-06-05 00:23:35 -0600

    build(deps): add luxon

--- client/cms/api.ts
+++ client/cms/api.ts
@@ -9,3 +9,11 @@ export const getPosts = async () => {
   const data = await client.fetch(`*[_type == "post"]`);
   return data;
 };
+
+export const getPost = async (slug: string) => {
+  const data = await client.fetch(
+    `*[_type == "post" && slug.current == $slug]`,
+    { slug }
+  );
+  return data[0];
+};
