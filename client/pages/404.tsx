04bc6d487ff54fa7fe155d9ceef7f7a0bada65d0
kito0 <drewshah0@gmail.com>
2022-06-11 15:33:03 -0600

    feat: create 404 page

---
+++ client/pages/404.tsx
@@ -0,0 +1,16 @@
+const Custom404 = () => {
+  return (
+    <section>
+      <div className='container'>
+        <h1>This is a 404 page</h1>
+        <p>
+          If you’re seeing this, I probably messed up somewhere. Yeah, I know,
+          the error code starts with a “4”, but c’mon, we both know I’m not
+          really fooling anybody right?
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Custom404;
