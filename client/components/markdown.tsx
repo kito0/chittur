528e54a408966a7860e28d559becdb6edb3bebeb
kito0 <drewshah0@gmail.com>
2022-06-11 00:14:34 -0600

    feat: add syntax highlighting

--- client/components/markdown.tsx
+++ client/components/markdown.tsx
@@ -1,11 +1,35 @@
 import 'katex/dist/katex.min.css';
 import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import rehypeKatex from 'rehype-katex';
 import remarkMath from 'remark-math';
 
 export const Markdown = ({ children }: { children: string }) => {
   return (
-    <ReactMarkdown remarkPlugins={[remarkMath]} rehypePlugins={[rehypeKatex]}>
+    <ReactMarkdown
+      remarkPlugins={[remarkMath]}
+      rehypePlugins={[rehypeKatex]}
+      components={{
+        code({ inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || '');
+          return !inline && match ? (
+            <SyntaxHighlighter
+              style={atomDark}
+              language={match[1]}
+              PreTag='div'
+              // {...props}
+            >
+              {String(children).replace(/\r?\n$/, '')}
+            </SyntaxHighlighter>
+          ) : (
+            <code className={className} {...props}>
+              {children}
+            </code>
+          );
+        },
+      }}
+    >
       {children}
     </ReactMarkdown>
   );
